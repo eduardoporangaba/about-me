9844447b1a876f12398f54440b0a788767499ca9
eduardoporangaba <eduardoporangaba1@gmail.com>
2025-12-19 08:10:55 -0300

    Adição da nova tela

--- app/dududu-edu/page.tsx
+++ app/dududu-edu/page.tsx
@@ -4,20 +4,19 @@ export default function DududuEdu() {
   return (
     <div className="min-h-screen bg-background font-mono tracking-wide flex flex-col items-center justify-center text-center">
       <h1 className="text-4xl font-bold mb-8">
-        Bem-vindo à página DuDudu&Edu!
+        Página em desenvolvimento . . .
       </h1>
+      <h2 className="text-2xl sm:text-3xl font-bold mb-2"></h2>
       <p className="text-lg max-w-2xl">
-        Aqui vai o conteúdo da sua nova tela. Pode adicionar vídeos, textos ou o
-        que quiser.
+        Apresentação de projetos.
         <br />
-        Exemplo: Um link para o vídeo original:
         <a
           href="https://www.instagram.com/palmeiras/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-purple-300 underline"
         >
-          Acesse Já
+          SE Palmeiras
         </a>
       </p>
       {/* Adicione mais elementos aqui, como embeds, imagens, etc. */}
